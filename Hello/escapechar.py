5f1049097f8acf32fc1019723c2c08386bdb8051
chadwyck242 <chadwyck.codeart@gmail.com>
2020-01-22 16:34:33 -0700

    first commit

---
+++ Hello/escapechar.py
@@ -0,0 +1,10 @@
+splitString = "This string has been \nsplit \nover several \nlines"
+print(splitString)
+
+tabString = "S\tH\tO\tE"
+print(tabString)
+
+print("C:\\Users\\chadsisk\\notes.txt")
+print(r"C:\Users\chadsisk\notes.txt")
+
+
---
+++ Hello/strings.py
@@ -0,0 +1,11 @@
+greeting = "Hello"
+name = "Chadwyck"
+
+print(greeting + ' ' + name)
+age = 44
+print(age)
+
+print(type(greeting))
+print(type(name))
+print(type(age))
+
